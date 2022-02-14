dd0a5502f1a8d6e8f09742ee3590082cc5c3eea2
Prakruti Gandhi <prakruti.gandhi@integrella.com>
2022-02-14 11:23:21 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Tasks from './components/Tasks'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container' style={{margin:'30px 6px'}}>
+      <h1>TO-DO-APP</h1>
+        <Tasks/>
+      </div>
   );
 }
 
